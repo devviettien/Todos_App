a68d17b2e28bea64878663d51f6c2ee6c87d32ce
devviettien <devbuinhathoang@gmail.com>
2023-06-26 17:42:28 +0700

    Initial Commit

---
+++ visionpro/visionproApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionproApp.swift
+//  visionpro
+//
+//  Created by Nhật Hoàng on 26/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct visionproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
